2941a2f21de895676a57d0579ea1a1e8d0cef095
jeremy <jeremy@jfein.net>
2017-06-30 14:46:53 -0500

    comments for notification class

--- src/CommunityVoices/Model/Component/Notifier.php
+++ src/CommunityVoices/Model/Component/Notifier.php
@@ -2,6 +2,10 @@
 
 namespace CommunityVoices\Model\Component;
 
+/**
+ * Handles error notification
+ */
+
 use OutOfBoundsException;
 use Exception;
 
@@ -9,15 +13,24 @@ use CommunityVoices\Model\Contract\ErrorNotifier;
 
 class Notifier implements ErrorNotifier
 {
-    private $errors = [];
+    private $collector = [];
 
     private $notifier;
 
+    /**
+     * Sets the error notifier to index new errors under
+     * @param strinrg $notifier Notifier label
+     */
     public function setNotifier($notifier)
     {
         $this->notifier = (string) $notifier;
     }
 
+    /**
+     * Adds an error to the collector
+     * @param string $key Reference key
+     * @param string $message Error message
+     */
     public function addError($key, $message)
     {
         if(is_null($this->notifier)) {
@@ -28,33 +41,46 @@ class Notifier implements ErrorNotifier
             throw new Exception('Notification key not specified.');
         }
 
-        if(!array_key_exists($this->notifier, $this->errors)) {
-            $this->errors[$this->notifier] = [];
+        if(!array_key_exists($this->notifier, $this->collector)) {
+            $this->collector[$this->notifier] = [];
         }
 
-        $this->errors[$this->notifier][$key] = $message;
+        $this->collector[$this->notifier][$key] = $message;
     }
 
+    /**
+     * Detects errors
+     * @return bool Boolean indicating if there are errors
+     */
     public function hasErrors(): bool
     {
-        return count($this->errors) > 0;
+        return count($this->collector) > 0;
     }
 
+    /**
+     * Gets errors from specific notifier
+     * @param  string $notifier Notifier label
+     * @return Array Collection of errors
+     */
     public function getErrorsByNotifier($notifier)
     {
-        if(!array_key_exists($notifier, $this->errors)) {
+        if(!array_key_exists($notifier, $this->collector)) {
             throw new OutOfBoundsException('No notifier "'.$notifier.'"');
         }
 
-        return $this->errors[$notifier];
+        return $this->collector[$notifier];
     }
 
+    /**
+     * Gets all errors
+     * @return Array Collection of errors
+     */
     public function getErrors()
     {
-        if(count($this->errors) <= 0) {
+        if(count($this->collector) <= 0) {
             return ;
         }
 
-        return $this->errors;
+        return $this->collector;
     }
 }
