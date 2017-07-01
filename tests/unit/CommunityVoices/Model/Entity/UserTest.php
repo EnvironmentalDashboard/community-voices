da3a1b340562b7e81259c704007a924d9c23a99d
jeremy <jeremy@jfein.net>
2017-06-30 20:13:08 -0500

    increase user test coverage

--- tests/unit/CommunityVoices/Model/Entity/UserTest.php
+++ tests/unit/CommunityVoices/Model/Entity/UserTest.php
@@ -2,6 +2,8 @@
 
 namespace CommunityVoices\Model\Entity;
 
+use CommunityVoices\Model\Component\Notifier;
+use CommunityVoices\Model\Exception\IdentityKnown;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
@@ -44,5 +46,44 @@ class UserTest extends TestCase
         $instance->setRole(User::ROLE_ADMIN);
 
         $this->assertSame($instance->getRole(), User::ROLE_ADMIN);
+    }
+
+    public function testIfValidUserIsValidForRegistration()
+    {
+        $mockNotifier = $this->createMock(Notifier::class);
+
+        $instance = new User;
+        $instance->setEmail('blah@foo.com');
+
+        $this->assertTrue($instance->isValidForRegistration($mockNotifier));
+    }
+
+    public function testIfInvalidForForRegistrationInvalidEmail()
+    {
+        $mockNotifier = $this
+                        ->getMockBuilder(Notifier::class)
+                        ->setMethods(['addError'])
+                        ->getMock();
+        $mockNotifier
+            ->expects($this->once())
+            ->method('addError')
+            ->with($this->equalTo('email'), $this->equalTo(User::ERR_INVALID_EMAIL));
+
+        $instance = new User;
+        $instance->setEmail('invalidemail');
+
+        $this->assertFalse($instance->isValidForRegistration($mockNotifier));
+    }
+
+    public function testIfInvalidForRegistrationIdentityKnown()
+    {
+        $this->expectException(IdentityKnown::class);
+
+        $mockNotifier = $this->createMock(Notifier::class);
+
+        $instance = new User;
+        $instance->setId(6);
+
+        $instance->isValidForRegistration($mockNotifier);
     }
 }
