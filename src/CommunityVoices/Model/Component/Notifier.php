6eeb2ecbc8e7bcf4ef2741b2a52db1bfa43ec0f8
jeremy <jeremy@jfein.net>
2017-07-05 16:01:06 -0500

    Added method `hasError` and reformatted

--- src/CommunityVoices/Model/Component/Notifier.php
+++ src/CommunityVoices/Model/Component/Notifier.php
@@ -33,15 +33,15 @@ class Notifier implements ErrorNotifier
      */
     public function addError($key, $message)
     {
-        if(is_null($this->notifier)) {
+        if (is_null($this->notifier)) {
             throw new Exception('Notification notifier specified');
         }
 
-        if(is_null($key)) {
+        if (is_null($key)) {
             throw new Exception('Notification key not specified.');
         }
 
-        if(!array_key_exists($this->notifier, $this->collector)) {
+        if (!array_key_exists($this->notifier, $this->collector)) {
             $this->collector[$this->notifier] = [];
         }
 
@@ -58,13 +58,28 @@ class Notifier implements ErrorNotifier
     }
 
     /**
+     * Detects specific error
+     * @param  string  $key Error key to search for
+     * @param  string  $message Error message to search for
+     * @return boolean Boolean indicating if there are errors
+     */
+    public function hasError($key, $message = null): bool
+    {
+        if (!$message) {
+            return array_key_exists($key, $this->collector);
+        }
+
+        return array_key_exists($key, $this->collector) && $this->collector[$key] === $message;
+    }
+
+    /**
      * Gets errors from specific notifier
      * @param  string $notifier Notifier label
      * @return Array Collection of errors
      */
     public function getErrorsByNotifier($notifier)
     {
-        if(!array_key_exists($notifier, $this->collector)) {
+        if (!array_key_exists($notifier, $this->collector)) {
             throw new OutOfBoundsException('No notifier "'.$notifier.'"');
         }
 
