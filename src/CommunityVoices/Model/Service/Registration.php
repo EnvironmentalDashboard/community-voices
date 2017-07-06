6436a3e4fcd8e883fa624079a5ecfb50ae8441be
jeremy <jeremy@jfein.net>
2017-07-06 11:21:10 -0500

    Finishing up registration service

--- src/CommunityVoices/Model/Service/Registration.php
+++ src/CommunityVoices/Model/Service/Registration.php
@@ -13,20 +13,19 @@ class Registration
 
     private $mapperFactory;
 
-    private $notifier;
-
     public function __construct(
         Palladium\Service\Registration $pdRegistration,
-        Component\MapperFactory $mapperFactory,
-        Component\Notifier $notifier
+        Component\MapperFactory $mapperFactory
     ) {
         $this->pdRegistration = $pdRegistration;
         $this->mapperFactory = $mapperFactory;
-        $this->notifier = $notifier;
     }
 
     public function createUser($email, $password, $confirmPassword, $firstName, $lastName)
     {
+        /**
+         * Create user entity and set attributes
+         */
         $user = new Entity\User;
 
         $user->setEmail($email);
@@ -36,22 +35,46 @@ class Registration
         $user->setLastName($lastName);
         $user->setRole($user::ROLE_UNVERIFIED);
 
+
+        /**
+         * Create error observer, set notifier, and pass it to user validator
+         */
+        $notifier = new Entity\Notifier;
+
         $notifier->setNotifier('registration');
 
         $isValid = $user->validateForRegistration($notifier);
 
-        if (!$isValid && $notifier->hasError('email', $user::ERR_EMAIL_INVALID)) {
-            return ; // @TODO save state to session (through abstraction)
+        $clientState = $this->mapperFactory->create(Mapper\ApplicationState::class);
+
+        /**
+         * Stop the registration process and save the errors to applicatin state
+         * if email is invalid. No point in continuing the validation process in
+         * making sure no user has this email if the email is invalid anyway
+         */
+        if (!$isValid && $notifier->hasEntry('email', $user::ERR_EMAIL_INVALID)) {
+            $clientState->save($notifier);
+            return ;
         }
 
         $userMapper = $this->mapperFactory->create(Mapper\User::class);
 
-        if($userMapper->existingUserWithEmail($user)) {
-            $notifier->addError('email', $user::ERR_EMAIL_EXISTS);
-            return ; // @TODO save state to session (through abstraction)
+        if ($userMapper->existingUserWithEmail($user)) {
+            $notifier->addEntry('email', $user::ERR_EMAIL_EXISTS);
         }
 
+        /**
+         * If there are any errors at this point, save the error state and stop
+         * the registration process
+         */
+        if ($notifier->hasEntries()) {
+            $clientState->save($notifier);
+            return ;
+        }
 
+        /**
+         * Register this user; save with the user mapper and with Palladium
+         */
         $userMapper->save($user);
 
         $identity = $this->pdRegistration->createEmailIdentity($email, $password);
