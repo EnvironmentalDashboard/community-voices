607894c32f9a865bb79cbdff73c2ee8d318357a1
jeremy <jeremy@jfein.net>
2017-07-10 15:29:24 -0500

    Unit testing

---
+++ tests/unit/CommunityVoices/Model/Mapper/CacheTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace CommunityVoices\Model\Mapper;
+
+use PHPUnit\Framework\TestCase;
+use CommunityVoices\Model\Entity;
+
+class CacheTest extends TestCase
+{
+    public function test_Fetching_Non_Existent_Instance()
+    {
+        $instance = $this->createMock(Entity\User::class);
+
+        $mapper = new Cache;
+
+        $this->assertFalse($mapper->fetch($instance));
+    }
+
+    public function test_Saving_Retrieving_Instance()
+    {
+        $instance = new Entity\User;
+        $instance->setId(5);
+        $instance->setFirstName('Foo');
+
+        $mapper = new Cache;
+        $mapper->save($instance);
+
+        $otherInstance = new Entity\User;
+        $otherInstance->setId(5);
+
+        $mapper->fetch($otherInstance);
+
+        $this->assertSame($instance->getFirstName(), $otherInstance->getFirstName());
+    }
+
+    public function test_Deleting_Cached_Instance()
+    {
+        $instance = new Entity\User;
+        $instance->setId(5);
+        $instance->setFirstName('Foo');
+
+        $mapper = new Cache;
+        $mapper->save($instance);
+
+        $mapper->delete($instance);
+
+        $this->assertFalse($mapper->fetch($instance));
+    }
+
+    public function test_Search_Existing_Instance()
+    {
+        $instance = new Entity\User;
+        $instance->setId(5);
+        $instance->setFirstName('Foo');
+
+        $mapper = new Cache;
+        $mapper->save($instance);
+
+
+        $otherInstance1 = new Entity\User;
+        $otherInstance1->setId(5);
+
+        $otherInstance2 = new Entity\User;
+        $otherInstance2->setId(4);
+
+
+        $this->assertTrue($mapper->exists($otherInstance1));
+        $this->assertFalse($mapper->exists($otherInstance2));
+    }
+}
---
+++ tests/unit/CommunityVoices/Model/Service/RegistrationTest.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace CommunityVoices\Model\Service;
+
+use PHPUnit\Framework\TestCase;
+use CommunityVoices\Model\Contract\HasId;
+use CommunityVoices\Model\Component;
+use CommunityVoices\Model\Entity;
+use CommunityVoices\Model\Mapper;
+use Palladium;
+
+class RegistrationTest extends TestCase
+{
+    public function test_Clean_Registration()
+    {
+        $sessionMapper = $this->createMock(Mapper\ApplicationState::class);
+
+        $userMapper = $this->createMock(Mapper\User::class);
+
+        $userMapper
+            ->method('save')
+            ->will($this->returnCallback(function (HasId $user) {
+                $user->setId(7);
+            }));
+
+        $mapperFactory = $this->createMock(Component\MapperFactory::class);
+
+        $mapperFactory
+            ->method('createSessionMapper')
+            ->with($this->equalTo(Mapper\ApplicationState::class))
+            ->will($this->returnValue($sessionMapper));
+
+        $mapperFactory
+            ->method('createDataMapper')
+            ->with($this->equalTo(Mapper\User::class))
+            ->will($this->returnValue($userMapper));
+
+        $pdIdentity = $this->createMock(Palladium\Entity\EmailIdentity::class);
+
+        $pdRegistration = $this->createMock(Palladium\Service\Registration::class);
+
+        $pdRegistration
+            ->method('createEmailIdentity')
+            ->will($this->returnValue($pdIdentity));
+
+        $registration = new Registration($pdRegistration, $mapperFactory);
+
+        $this->assertTrue($registration->createUser(
+            'john@doe.com',
+            'password123',
+            'password123',
+            'John',
+            'Doe'
+        ));
+    }
+
+    public function test_Registration_Invalid_Email()
+    {
+        $sessionMapper = $this->createMock(Mapper\ApplicationState::class);
+
+        $mapperFactory = $this->createMock(Component\MapperFactory::class);
+
+        $mapperFactory
+            ->method('createSessionMapper')
+            ->with($this->equalTo(Mapper\ApplicationState::class))
+            ->will($this->returnValue($sessionMapper));
+
+        $pdIdentity = $this->createMock(Palladium\Entity\EmailIdentity::class);
+
+        $pdRegistration = $this->createMock(Palladium\Service\Registration::class);
+
+        $pdRegistration
+            ->method('createEmailIdentity')
+            ->will($this->returnValue($pdIdentity));
+
+        $registration = new Registration($pdRegistration, $mapperFactory);
+
+        $this->assertFalse($registration->createUser(
+            'johndoe.com',
+            'password123',
+            'password123',
+            'John',
+            'Doe'
+        ));
+    }
+
+    public function test_Create_User_Duplicate_Email()
+    {
+        $sessionMapper = $this->createMock(Mapper\ApplicationState::class);
+
+        $userMapper = $this->createMock(Mapper\User::class);
+
+        $userMapper
+            ->method('existingUserWithEmail')
+            ->will($this->returnValue(true));
+
+        $mapperFactory = $this->createMock(Component\MapperFactory::class);
+
+        $mapperFactory
+            ->method('createSessionMapper')
+            ->with($this->equalTo(Mapper\ApplicationState::class))
+            ->will($this->returnValue($sessionMapper));
+
+        $mapperFactory
+            ->method('createDataMapper')
+            ->with($this->equalTo(Mapper\User::class))
+            ->will($this->returnValue($userMapper));
+
+        $pdIdentity = $this->createMock(Palladium\Entity\EmailIdentity::class);
+
+        $pdRegistration = $this->createMock(Palladium\Service\Registration::class);
+
+        $pdRegistration
+            ->method('createEmailIdentity')
+            ->will($this->returnValue($pdIdentity));
+
+        $registration = new Registration($pdRegistration, $mapperFactory);
+
+        $this->assertFalse($registration->createUser(
+            'john@doe.com',
+            'password123',
+            'password123',
+            'John',
+            'Doe'
+        ));
+    }
+}
