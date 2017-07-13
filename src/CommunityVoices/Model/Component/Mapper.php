903b696e56df82916b3ea10c01490431240a3dbd
jeremy <jeremy@jfein.net>
2017-07-13 10:40:09 -0500

    Add method convertRelationsToEntities

--- src/CommunityVoices/Model/Component/Mapper.php
+++ src/CommunityVoices/Model/Component/Mapper.php
@@ -26,4 +26,30 @@ class Mapper
             }
         }
     }
+
+    /**
+     * Runs over $parameters and replaces any ID relations indicated by keys in
+     * $relations with new instance entities;
+     *
+     * @param  array  $relations A key-value array of the relations to convert
+     * @param  array  $parameters Array of entity parameters
+     * @return array New parameter array
+     */
+    public function convertRelationsToEntities(array $relations, array $parameters)
+    {
+        foreach ($relations as $key => $value) {
+            $key = strtolower(preg_replace('/([^A-Z])([A-Z])/', "$1_$2", $key));
+
+            if (!array_key_exists($key, $parameters)) {
+                continue;
+            }
+
+            $instance = new $value;
+            $instance->setId($parameters[$key]);
+
+            $parameters[$key] = $instance;
+        }
+
+        return $parameters;
+    }
 }
