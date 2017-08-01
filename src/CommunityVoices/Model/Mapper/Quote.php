8b7b5bd746d95db0de8898ae893d4bbcdf83a0b2
jeremy <jeremy@jfein.net>
2017-08-01 14:35:42 -0500

    Quote mapper & mapper test

---
+++ src/CommunityVoices/Model/Mapper/Quote.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace CommunityVoices\Model\Mapper;
+
+use PDO;
+use CommunityVoices\Model\Component\DataMapper;
+use CommunityVoices\Model\Entity as Entity;
+
+class Quote extends Media
+{
+    protected static $table = '`community-voices_quote`';
+
+    public function fetch(Entity\Media $quote)
+    {
+        $this->fetchById($quote);
+    }
+
+    private function fetchById(Entity\Media $quote)
+    {
+        $query = "SELECT    parent.id,
+                            parent.added_by,
+                            parent.date_created,
+                            parent.type,
+                            parent.status,
+                            child.text,
+                            child.attribution,
+                            child.date_recorded,
+                            child.public_document_link,
+                            child.source_document_link
+                    FROM    " . parent::$table . " parent
+                    JOIN    " . self::$table . " child
+                    ON      parent.id = child.media_id
+                    WHERE   parent.id = :id";
+
+        $statement = $this->conn->prepare($query);
+
+        $statement->bindValue(':id', $quote->getId());
+
+        $statement->execute();
+
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+        if ($result) {
+            $parameters = $this->convertRelationsToEntities($quote->getRelations(), $result);
+
+            $this->applyValues($quote, $parameters);
+        }
+    }
+
+    public function save(Entity\Media $quote)
+    {
+        if ($quote->getId()) {
+            $this->update($quote);
+            return ;
+        }
+
+        $this->create($quote);
+    }
+
+    protected function update(Entity\Media $quote)
+    {
+        parent::update($quote);
+
+        $query = "UPDATE    " . self::$table . "
+                    SET     text = :text,
+                            attribution = :attribution,
+                            date_recorded = :date_recorded,
+                            public_document_link = :public_document_link,
+                            source_document_link = :source_document_link
+                    WHERE   media_id = :media_id";
+
+        $statement = $this->conn->prepare($query);
+
+        $statement->bindValue(':media_id', $quote->getId());
+        $statement->bindValue(':text', $quote->getText());
+        $statement->bindValue(':attribution', $quote->getAttribution());
+        $statement->bindValue(':date_recorded', $quote->getDateRecorded());
+        $statement->bindValue(':public_document_link', $quote->getPublicDocumentLink());
+        $statement->bindValue(':source_document_link', $quote->getSourceDocumentLink());
+
+        $statement->execute();
+    }
+
+    protected function create(Entity\Media $quote)
+    {
+        parent::create($quote);
+
+        $query = "INSERT INTO   " . self::$table . "
+                                (media_id, text, attribution, date_recorded, public_document_link,
+                                    source_document_link)
+
+                    VALUES      (:media_id, :text, :attribution, :date_recorded, :public_document_link,
+                                    :source_document_link)";
+
+        $statement = $this->conn->prepare($query);
+
+        $statement->bindValue(':media_id', $quote->getId());
+        $statement->bindValue(':text', $quote->getText());
+        $statement->bindValue(':attribution', $quote->getAttribution());
+        $statement->bindValue(':date_recorded', $quote->getDateRecorded());
+        $statement->bindValue(':public_document_link', $quote->getPublicDocumentLink());
+        $statement->bindValue(':source_document_link', $quote->getSourceDocumentLink());
+
+        $statement->execute();
+    }
+
+    public function delete(Entity\Media $quote)
+    {
+        parent::delete($quote); //deletion cascades
+    }
+}
---
+++ tests/unit/CommunityVoices/Model/Mapper/QuoteTest.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace CommunityVoices\Model\Mapper;
+
+use PDO;
+use PDOStatement;
+use PHPUnit\Framework\TestCase;
+use CommunityVoices\Model\Entity;
+
+/**
+ * @covers CommunityVoices\Model\Mapper\Quote
+ */
+class QuoteTest extends TestCase
+{
+    public function test_Retrieving_Quote_By_Id()
+    {
+        $pdo = $this
+            ->getMockBuilder(PDO::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $statement = $this
+            ->getMockBuilder(PDOStatement::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $statement
+            ->method('bindValue')
+            ->with($this->equalTo(':id'), $this->equalTo(2));
+
+        $statement
+            ->expects($this->once())
+            ->method('fetch')
+            ->will($this->returnValue([
+                'id' => 8,
+                'added_by' => 6
+            ]));
+
+        $pdo
+            ->expects($this->once())
+            ->method('prepare')
+            ->will($this->returnValue($statement));
+
+        $quote = new Entity\Quote;
+        $quote->setId(2);
+
+        $mapper = new Quote($pdo);
+        $mapper->fetch($quote);
+
+        $this->assertSame($quote->getId(), 8);
+        $this->assertEquals($quote->getAddedBy()->getId(), 6);
+    }
+
+    public function test_Creating_Quote()
+    {
+        $creator = new Entity\User;
+        $creator->setId(1);
+
+        $quote = new Entity\Quote;
+        $quote->setAddedBy($creator);
+        $quote->setStatus($quote::STATUS_APPROVED);
+        $quote->setText('Lorem ipsum');
+        $quote->setAttribution('John Doe');
+        $quote->setDateRecorded(time());
+        $quote->setPublicDocumentLink('http://localhost:1');
+        $quote->setSourceDocumentLink('http://localhost:2');
+
+        $pdo = $this
+            ->getMockBuilder(PDO::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $pdo
+            ->expects($this->once())
+            ->method('lastInsertId')
+            ->will($this->returnValue(2));
+
+        $statementByParent = $this
+            ->getMockBuilder(PDOStatement::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $statementByParent
+            ->method('bindValue')
+            ->withConsecutive(
+                [$this->equalTo(':added_by'), $this->equalTo($quote->getAddedBy()->getId())],
+                [$this->equalTo(':date_created'), $this->equalTo(time())],
+                [$this->equalTo(':type'), $this->equalTo($quote->getType())],
+                [$this->equalTo(':status'), $this->equalTo($quote->getStatus())]
+            );
+
+        $statementByChild = $this
+            ->getMockBuilder(PDOStatement::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $statementByChild
+            ->method('bindValue')
+            ->withConsecutive(
+                [$this->equalTo(':media_id'), $this->equalTo(2)],
+                [$this->equalTo(':text'),
+                 $this->equalTo($quote->getText())],
+                [$this->equalTo(':attribution'), $this->equalTo($quote->getAttribution())],
+                [$this->equalTo(':date_recorded'), $this->equalTo($quote->getDateRecorded())],
+                [$this->equalTo(':public_document_link'), $this->equalTo($quote->getPublicDocumentLink())],
+                [$this->equalTo(':source_document_link'), $this->equalTo($quote->getSourceDocumentLink())]
+            );
+
+        $pdo
+            ->expects($this->exactly(2))
+            ->method('prepare')
+            ->will($this->onConsecutiveCalls(
+                $statementByParent,
+                $statementByChild
+            ));
+
+        $mapper = new Quote($pdo);
+        $mapper->save($quote);
+    }
+
+    public function test_Updating_Quote()
+    {
+        $creator = new Entity\User;
+        $creator->setId(1);
+
+        $quote = new Entity\Quote;
+        $quote->setId(2);
+        $quote->setAddedBy($creator);
+        $quote->setStatus($quote::STATUS_APPROVED);
+        $quote->setText('Lorem ipsum');
+        $quote->setAttribution('John Doe');
+        $quote->setDateRecorded(time());
+        $quote->setPublicDocumentLink('http://localhost:1');
+        $quote->setSourceDocumentLink('http://localhost:2');
+
+        $pdo = $this
+            ->getMockBuilder(PDO::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $statementByParent = $this
+            ->getMockBuilder(PDOStatement::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $statementByParent
+            ->method('bindValue')
+            ->withConsecutive(
+                [$this->equalTo(':id'), $this->equalTo($quote->getId())],
+                [$this->equalTo(':added_by'), $this->equalTo($quote->getAddedBy()->getId())],
+                [$this->equalTo(':type'), $this->equalTo($quote->getType())],
+                [$this->equalTo(':status'), $this->equalTo($quote->getStatus())]
+            );
+
+        $statementByChild = $this
+            ->getMockBuilder(PDOStatement::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $statementByChild
+            ->method('bindValue')
+            ->withConsecutive(
+                [$this->equalTo(':media_id'), $this->equalTo(2)],
+                [$this->equalTo(':text'),
+                 $this->equalTo($quote->getText())],
+                [$this->equalTo(':attribution'), $this->equalTo($quote->getAttribution())],
+                [$this->equalTo(':date_recorded'), $this->equalTo($quote->getDateRecorded())],
+                [$this->equalTo(':public_document_link'), $this->equalTo($quote->getPublicDocumentLink())],
+                [$this->equalTo(':source_document_link'), $this->equalTo($quote->getSourceDocumentLink())]
+            );
+
+        $pdo
+            ->expects($this->exactly(2))
+            ->method('prepare')
+            ->will($this->onConsecutiveCalls(
+                $statementByParent,
+                $statementByChild
+            ));
+
+        $mapper = new Quote($pdo);
+        $mapper->save($quote);
+    }
+
+    public function test_Deleting_Quote()
+    {
+        $quote = new Entity\Quote;
+        $quote->setId(3);
+
+        $pdo = $this
+            ->getMockBuilder(PDO::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $statement = $this
+            ->getMockBuilder(PDOStatement::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $statement
+            ->method('bindValue')
+            ->with($this->equalTo(':id'), $this->equalTo($quote->getId()));
+
+        $pdo
+            ->expects($this->once())
+            ->method('prepare')
+            ->will($this->returnValue($statement));
+
+        $mapper = new Quote($pdo);
+        $mapper->delete($quote);
+    }
+}
