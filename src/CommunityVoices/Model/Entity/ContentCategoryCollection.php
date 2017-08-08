d07db73f0fcf3b18a528d7b8e81aa9de75e16faa
jeremy <jeremy@jfein.net>
2017-08-08 15:35:43 -0500

    ContentCategoryCollection implementation

---
+++ src/CommunityVoices/Model/Entity/ContentCategoryCollection.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CommunityVoices\Model\Entity;
+
+use CommunityVoices\Model\Contract\HasId;
+
+class ContentCategoryCollection extends GroupCollection
+{
+    /**
+     * @codeCoverageIgnore
+     */
+    protected function makeEntity()
+    {
+        return new ContentCategory;
+    }
+
+    public function __construct()
+    {
+        $this->groupType = self::GROUP_TYPE_CONT_CAT;
+    }
+}
