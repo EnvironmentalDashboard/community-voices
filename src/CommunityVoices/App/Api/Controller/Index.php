b4c0a7dca37e369fe9a93a34186483220ff7cd33
jeremy <jeremy@jfein.net>
2017-09-03 15:56:47 -0400

    Adding parent CommunityVoices dir to simplify instantiation/namespacing

---
+++ src/CommunityVoices/App/Api/Controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CommunityVoices\App\Controller;
+
+class Index
+{
+    public function getLanding()
+    {
+        /**
+         * Process request
+         */
+    }
+}
---
+++ src/CommunityVoices/App/Api/View/Index.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace CommunityVoices\App\View;
+
+use CommunityVoices\App\HTMLPresentation;
+
+class Index
+{
+    public function getLanding()
+    {
+        /**
+         * Prepare presentation
+         */
+
+        //$images = $this->lookup->findImages();
+        //$authentication = $request->getAuthentication();
+
+        //$processor = new OneStepProcessor(HTMLPresentation/ImageCollection::class);
+    }
+}
