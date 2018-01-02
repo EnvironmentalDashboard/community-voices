dc849ba6daac97027f45f76366f6ffed9f487321
jeremy <jeremy@jfein.net>
2018-01-02 09:23:11 -0600

    Emptying implementation

--- src/CommunityVoices/Model/Component/Exception.php
+++ src/CommunityVoices/Model/Component/Exception.php
@@ -4,11 +4,4 @@ namespace CommunityVoices\Model\Component;
 
 class Exception extends \Exception
 {
-    /**
-     * @codeCoverageIgnore
-     */
-    public function __construct($message, $code = 0, Exception $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
 }
