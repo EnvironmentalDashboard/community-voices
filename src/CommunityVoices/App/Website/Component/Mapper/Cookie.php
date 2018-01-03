c24a58a64afd444c1eed0f4f529c610a5b0d07d5
jeremy <jeremy@jfein.net>
2018-01-02 18:01:40 -0600

    Updating cookie mapper for new request/response abstraction

--- src/CommunityVoices/App/Website/Component/Mapper/Cookie.php
+++ src/CommunityVoices/App/Website/Component/Mapper/Cookie.php
@@ -3,30 +3,45 @@
 namespace CommunityVoices\App\Website\Component\Mapper;
 
 use CommunityVoices\Model\Contract;
+use Symfony\Component\HttpFoundation\Cookie;
 
 class Cookie extends \CommunityVoices\Model\Component\Mapper
 {
     private $request;
     private $response;
 
-    public function __construct($request, $response)
+    private $setCookies = [];
+    private $clearCookies = [];
+
+    public function __construct($request, $response = null)
     {
         $this->request = $request;
         $this->response = $response;
     }
 
-    public function save(Contract\Cookieable $instance)
+    public function provideResponseHandler($response)
     {
-        $this->response->addCookie(
-            $instance->getUniqueLabel(),
-            $instance->toJson(),
-            ['expires' => $instance->getExpiresOn()]
-        );
+        $this->response = $response;
+    }
+
+    public function mapToResponse()
+    {
+        if (!$this->response) {
+            throw new \Exception("No response handler.");
+        }
+
+        foreach ($this->setCookies as $key => $cookie) {
+            $this->response->headers->setCookie($cookie);
+        }
+
+        foreach ($this->clearCookies as $key => $name) {
+            $this->response->headers->clearCookie($name);
+        }
     }
 
     public function fetch(Contract\Cookieable $instance)
     {
-        $cookie = $this->request->getCookie($instance->getUniqueLabel());
+        $cookie = $this->request->cookies->get($instance->getUniqueLabel());
 
         if (!$cookie) {
             return false;
@@ -35,8 +50,23 @@ class Cookie extends \CommunityVoices\Model\Component\Mapper
         $this->populateEntity($instance, json_decode($cookie, true));
     }
 
+    public function save(Contract\Cookieable $instance)
+    {
+        $cookie = new Cookie(
+            $instance->getUniqueLabel(),
+            $instance->toJson(),
+            $instance->getExpiresOn()
+        );
+
+        $this->setCookies[] = $cookie;
+
+        $this->request->cookies->set($instance->getUniqueLabel(), $instance->toJson());
+    }
+
     public function delete(Contract\Cookieable $instance)
     {
-        $this->response->removeCookie($instance->getUniqueLabel());
+        $this->clearCookies[] = $instance->getUniqueLabel();
+
+        $this->request->cookies->remove($instance->getUniqueLabel());
     }
 }
