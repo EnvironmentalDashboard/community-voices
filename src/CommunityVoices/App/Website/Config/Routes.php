b0a070624675a1cd6d02ce35bebd321a1be9283b
jeremy <jeremy@jfein.net>
2018-01-02 18:02:33 -0600

    Routes file to work with Symfony router

---
+++ src/CommunityVoices/App/Website/Config/Routes.php
@@ -0,0 +1,33 @@
+<?php
+
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
+
+$appPrefix = '/community-voices';
+
+$collection = new RouteCollection();
+
+$collection->add(
+    'postLogin',
+    new Route('/authenticate', array('_controller' => 'ExampleController'))
+);
+
+$config = json_decode(file_get_contents(__DIR__ . '/Routes.json'), true);
+
+
+foreach ($config as $name => $options) {
+    $collection->add(
+        $name,
+        new Route(
+            $appPrefix . $options['notation'],
+            $options['defaults'],
+            isset($options['requirements']) ? $options['requirements'] : [],
+            [],
+            '',
+            [],
+            isset($options['defaults']['method']) ? $options['defaults']['method'] : []
+        )
+    );
+}
+
+return $collection;
