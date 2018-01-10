f82167c60ca6137cb5ae98998bee7c047548509e
jeremy <jeremy@jfein.net>
2018-01-10 13:12:05 -0600

    Added docblock to website.php

--- src/CommunityVoices/Public/website.php
+++ src/CommunityVoices/Public/website.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ *
+ * Web application main entry point
+ *
+ * Includes bootstrap
+ * @see ../App/Website/bootstrap.php
+ */
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
