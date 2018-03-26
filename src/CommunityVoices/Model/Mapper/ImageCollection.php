51e24c89f505ce983b74911e7a9e3217ae499c2e
ldreith <ldreith@oberlin.edu>
2018-03-25 22:39:13 -0400

    adding functionality to return media based off status in mapper layer

--- src/CommunityVoices/Model/Mapper/ImageCollection.php
+++ src/CommunityVoices/Model/Mapper/ImageCollection.php
@@ -9,9 +9,15 @@ use CommunityVoices\Model\Entity;
 
 class ImageCollection extends DataMapper
 {
-    public function fetch(Entity\ImageCollection $imageCollection)
+    public function fetch(Entity\ImageCollection $imageCollection, $status = 3)
     {
-        return $this->fetchAll($imageCollection);
+        if( $status == 3 ){
+            return $this->fetchAll($imageCollection);
+        } elseif( $status == 2 ) {
+            return $this->fetchRejected($imageCollection);
+        } elseif( $status == 1 ) {
+            return $this->fetchPending($imageCollection);
+        }
     }
 
     private function fetchAll(Entity\ImageCollection $imageCollection)
@@ -34,6 +40,43 @@ class ImageCollection extends DataMapper
 					INNER JOIN
 						`community-voices_images` image
 						ON media.id = image.media_id
+          WHERE
+            media.status = 'approved'
+				 ";
+
+        $statement = $this->conn->prepare($query);
+
+        $statement->execute();
+
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($results as $key => $entry) {
+            $imageCollection->addEntityFromParams($entry);
+        }
+    }
+
+    private function fetchPending(Entity\ImageCollection $imageCollection)
+    {
+        $query = " 	SELECT
+						media.id 						AS id,
+						media.added_by 					AS addedBy,
+						media.date_created 				AS dateCreated,
+                        CAST(media.type AS UNSIGNED)    AS type,
+                        CAST(media.status AS UNSIGNED)  AS status,
+                        image.filename                  AS filename,
+                        image.generated_tags            AS generatedTags,
+                        image.title                     AS title,
+                        image.description               AS description,
+                        image.date_taken                AS dateTaken,
+                        image.photographer              AS photographer,
+                        image.organization              AS organization
+					FROM
+						`community-voices_media` media
+					INNER JOIN
+						`community-voices_images` image
+						ON media.id = image.media_id
+          WHERE
+            media.status = 'pending'
 				 ";
 
         $statement = $this->conn->prepare($query);
--- src/CommunityVoices/Model/Mapper/QuoteCollection.php
+++ src/CommunityVoices/Model/Mapper/QuoteCollection.php
@@ -10,9 +10,15 @@ use CommunityVoices\Model\Entity;
 class QuoteCollection extends DataMapper
 {
 
-    public function fetch(Entity\QuoteCollection $quoteCollection)
+    public function fetch(Entity\QuoteCollection $quoteCollection, $status = 3)
     {
-        return $this->fetchAll($quoteCollection);
+        if( $status == 3 ){
+            return $this->fetchAll($quoteCollection);
+        } elseif( $status == 2 ){
+            return $this->fetchRejected($quoteCollection); // @TODO
+        } elseif( $status == 1 ){
+            return $this->fetchPending($quoteCollection);
+        }
     }
 
     private function fetchAll(Entity\QuoteCollection $quoteCollection)
@@ -36,6 +42,41 @@ class QuoteCollection extends DataMapper
 						ON media.id = quote.media_id
           WHERE
             media.status ='approved'
+				 "
+				 . $this->query_creators($quoteCollection->creators);
+
+        $statement = $this->conn->prepare($query);
+
+        $statement->execute();
+
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($results as $key => $entry) {
+            $quoteCollection->addEntityFromParams($entry);
+        }
+    }
+
+    private function fetchPending(Entity\QuoteCollection $quoteCollection)
+    {
+        $query = " 	SELECT
+						media.id 						AS id,
+						media.added_by 					AS addedBy,
+						media.date_created 				AS dateCreated,
+                        CAST(media.type AS UNSIGNED)    AS type,
+                        CAST(media.status AS UNSIGNED)  AS status,
+                        quote.text                      AS text,
+                        quote.attribution               AS attribution,
+                        quote.sub_attribution           AS subAttribution,
+                        quote.date_recorded             AS dateRecorded,
+                        quote.public_document_link      AS publicDocumentLink,
+                        quote.source_document_link      AS sourceDocumentLink
+					FROM
+						`community-voices_media` media
+					INNER JOIN
+						`community-voices_quotes` quote
+						ON media.id = quote.media_id
+          WHERE
+            media.status = 'pending'
 				 "
 				 . $this->query_creators($quoteCollection->creators);
 
