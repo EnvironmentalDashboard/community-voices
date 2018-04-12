279c6eee723562566d8be8f64911d8b9533312e8
Lars Dreith <ldreith@oberlin.edu>
2018-04-11 22:43:10 -0400

    default content-categories and tags for Oberlin

---
+++ migrate/defaults.sql
@@ -0,0 +1,247 @@
+/**
+ * Addding default Content Categories
+ */
+
+ INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (1, "Serving Our Community","content-category");
+ INSERT INTO `community-voices_content-categories` (`group_id`, `media_filename`) VALUES (1, "...");
+
+ INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (2, "Our Downtown","content-category");
+ INSERT INTO `community-voices_content-categories` (`group_id`, `media_filename`) VALUES (2, "...");
+
+ INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (3, "Next Generation","content-category");
+ INSERT INTO `community-voices_content-categories` (`group_id`, `media_filename`) VALUES (3, "...");
+
+ INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (4, "Heritage","content-category");
+ INSERT INTO `community-voices_content-categories` (`group_id`, `media_filename`) VALUES (4, "...");
+
+ INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (5, "Natural Oberlin","content-category");
+ INSERT INTO `community-voices_content-categories` (`group_id`, `media_filename`) VALUES (5, "...");
+
+ INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (6, "Neighbors","content-category");
+ INSERT INTO `community-voices_content-categories` (`group_id`, `media_filename`) VALUES (6, "...");
+
+ /**
+  * Adding default Tags
+  */
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (7, "Aeiral","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (7);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (8, "Agriculture","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (8);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (9, "AJLC","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (9);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (10, "Animal","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (10);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (11, "Appliance","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (11);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (12, "Art","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (12);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (13, "Artwork","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (13);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (14, "Bicycle","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (14);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (15, "Big Parade","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (15);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (16, "Building Dashboard","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (16);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (17, "Building","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (17);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (18, "Business","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (18);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (19, "Celebration","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (19);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (20, "Children","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (20);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (21, "City Infrastructure","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (21);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (22, "Civil Rights","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (22);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (23, "Climate Change","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (23);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (24, "College","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (24);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (25, "Conservation","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (25);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (26, "Digital Sign","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (26);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (27, "Dormitory","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (27);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (28, "Downtown","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (28);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (29, "DWTP","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (29);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (30, "Food","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (30);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (31, "Eastwood","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (31);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (32, "Ecolympics","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (32);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (33, "Education","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (33);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (34, "Electricity","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (34);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (35, "Energy","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (35);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (36, "Event","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (36);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (37, "Exercise","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (37);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (38, "Family","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (38);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (39, "Gardening","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (39);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (40, "GLCA","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (40);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (41, "GLPF","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (41);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (42, "Historical","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (42);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (43, "HVAC","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (43);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (44, "Interview","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (44);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (45, "Langston","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (45);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (46, "Living Machine","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (46);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (47, "Meeting","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (47);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (48, "Meter","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (48);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (49, "Monitoring","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (49);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (50, "Music","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (50);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (51, "Nature","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (51);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (52, "OCS","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (52);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (53, "OHS","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (53);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (54, "OMLPS","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (54);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (55, "OPL","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (55);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (56, "Plum Creek","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (56);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (57, "People","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (57);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (58, "Prospect","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (58);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (59, "Plants","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (59);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (60, "Recycling","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (60);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (61, "Renewable","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (61);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (62, "Residential","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (62);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (63, "Retrofit","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (63);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (64, "Seasons","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (64);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (65, "Spring","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (65);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (66, "Summer","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (66);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (67, "Fall","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (67);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (68, "Winter","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (68);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (69, "Rural","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (69);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (70, "Seniors","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (70);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (71, "Solar","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (71);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (72, "Student","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (72);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (73, "Tappan Square","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (73);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (74, "Teachers","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (74);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (75, "Transportation","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (75);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (76, "Volunteering","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (76);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (77, "Water","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (77);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (78, "Weather","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (78);
+
+  INSERT INTO `community-voices_groups`(`id`, `label`, `type`) VALUES (79, "WWTP","tag");
+  INSERT INTO `community-voices_tags` (`group_id`) VALUES (79);
+
+  /**
+   * Adding default locations
+   */
