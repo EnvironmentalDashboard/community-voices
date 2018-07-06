53463e14b793d4a5ff746308981be495b681fc9a
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-07-06 10:04:12 -0400

    apparently this breaks stuff

--- src/CommunityVoices/Model/Entity/Image.php
+++ src/CommunityVoices/Model/Entity/Image.php
@@ -66,7 +66,7 @@ class Image extends Media
         return $this->dateTaken;
     }
 
-    public function setDateTaken(int $dateTaken)
+    public function setDateTaken($dateTaken)
     {
         $input = (int) $dateTaken;
 
