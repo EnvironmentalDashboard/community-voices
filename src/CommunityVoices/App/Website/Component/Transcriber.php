d0bb47ab247f0c5b1ccf752c32c4399b96cc3bfd
jeremy <jeremy@jfein.net>
2018-11-11 15:26:51 -0500

    Enforced transcriber responsibility to convert arrays to valid XML

--- src/CommunityVoices/App/Website/Component/Transcriber.php
+++ src/CommunityVoices/App/Website/Component/Transcriber.php
@@ -2,8 +2,21 @@
 
 namespace CommunityVoices\App\Website\Component;
 
+/**
+ * Responsible for transcribing arrays/objects into proper encoded XML
+ */
 class Transcriber
 {
+    /**
+     * Converts an associative array/object to XML using the following rules:
+     *  1. Lowest level array values are encoded for XML
+     *  2. Associative array keys are used as element names, while their values are
+     *     the elements value
+     *  3. Indexed arrays' values are not surrounded by elements
+     *
+     * @param  mixed $array Associative array/object to be converted
+     * @return string Output XML
+     */
     public function toXml($array)
     {
         $xml = '';
@@ -11,6 +24,8 @@ class Transcriber
         foreach ($array as $key => $value) {
             if (is_array($value) || is_object($value)) { //PHP 7.1 supports is_iterable
                 $value = $this->toXml($value);
+            } else {
+                $value = $this->encode($value);
             }
 
             if (is_int($key)) {
@@ -21,5 +36,10 @@ class Transcriber
         }
 
         return $xml;
+    }
+
+    private function encode($input)
+    {
+        return htmlspecialchars($output);
     }
 }
