77e115b5c1c42d1dff0d89ef74c02f94ec10654f
Sam Craig <craigsamm@gmail.com>
2018-12-31 13:10:34 -0500

    Add functional Exif view

--- src/CommunityVoices/App/Website/Controller/Exif.php
+++ src/CommunityVoices/App/Website/Controller/Exif.php
@@ -4,11 +4,8 @@ namespace CommunityVoices\App\Website\Controller;
 
 class Exif
 {
-	// can be get to actually see it
-	// try to set up console
 	public function postData ($request)
 	{
-		var_dump('hi');
+
 	}
 }
->
