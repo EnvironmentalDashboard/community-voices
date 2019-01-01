f2196763a090340b2b904190bd02a45106697a47
jeremy <jeremy@jfein.net>
2019-01-01 02:46:02 -0600

    use swift mailer in Emailer service

--- src/CommunityVoices/Model/Service/Emailer.php
+++ src/CommunityVoices/Model/Service/Emailer.php
@@ -6,44 +6,26 @@ namespace CommunityVoices\Model\Service;
  * @overview Basically just a wrapper for PHPMailer
  */
 
-use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+
+use Swift_Mailer;
+use Swift_Message;
 
 class Emailer
 {
-  
-  function __construct(PHPMailer\PHPMailer\PHPMailer $mail)
-  {
-    $mail->CharSet = 'UTF-8';
-    $mail->setFrom('no-reply@environmentaldashboard.org', 'Environmental Dashboard');
-    // This should be the same as the domain of your From address
-    $mail->DKIM_domain = 'environmentaldashboard.org';
-    // Path to your private key:
-    $mail->DKIM_private = '/opendkim/mail.private';
-    // Set this to your own selector
-    $mail->DKIM_selector = 'mail';
-    // Put your private key's passphrase in here if it has one
-    $mail->DKIM_passphrase = '';
-    // The identity you're signing as - usually your From address
-    $mail->DKIM_identity = $mail->From;
-    $this->mail = $mail;
-  }
+    private $swiftMailer;
+    private $swiftDkimSigner;
 
-  public function to(string $recipient) {
-    $this->mail->addAddress($recipient);
-  }
-
-  public function subject(string $subject) {
-    $this->mail->Subject = $subject;
-  }
-
-  public function sendMessage(string $html) {
-    $this->mail->msgHTML($html);
-    if (!$this->mail->send()) {
-      throw new Exception\Mailer($this->mail->ErrorInfo);
+    public function __construct(
+        Swift_Mailer $swiftMailer,
+        Swift_Signers_DKIMSigner $swiftDkimSigner
+    ) {
+        $this->swiftMailer = $swiftMailer;
+        $this->swiftDkimSigner = $swiftDkimSigner;
     }
-  }
 
-
+    public function send(Swift_Message $swiftMessage)
+    {
+        $swiftMessage->attachSigner($this->swiftDkimSigner);
+    }
 }
-
