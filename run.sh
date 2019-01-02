ec583890efa893a009d1a57d8fce8c782707a6dd
Sam Craig <craigsamm@gmail.com>
2019-01-02 16:08:15 -0500

    Store our database user, pass in a private file

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Read our password from our secret file.
+if [ -f "db.config" ]
+then
+	. db.config
+else
+	echo "You must have a database configuration!"
+	echo "Exiting without running."
+	exit
+fi
+
 if [ "$#" -gt 0 ]
 then
 	IFS="" # so newlines don't get lost with DKIM=`cat /etc/opendkim/keys/environmentaldashboard.org/mail.private`
@@ -7,7 +17,7 @@ then
 	docker run -dit -p 3002:80 --restart always \
 	-v /var/www/uploads/CV_Media/images/:/var/www/uploads/CV_Media/images/ \
 	-v $(pwd):/var/www/html/ \
-	-e "MYSQL_HOST=159.89.232.129" -e "MYSQL_DB=community_voices" -e "MYSQL_USER=public_cv" -e "MYSQL_PASS=1234" \
+	-e "MYSQL_HOST=159.89.232.129" -e "MYSQL_DB=community_voices" -e "MYSQL_USER=$user" -e "MYSQL_PASS=$pass" \
 	-e SERVER=`hostname` -e DKIM=`cat /etc/opendkim/keys/environmentaldashboard.org/mail.private` \
 	--name PROD_CV community-voices
 else
@@ -15,7 +25,7 @@ else
 	docker run -dit -p 3002:80 --restart always \
 	-v $(pwd)/CV_Media/images/:/var/www/uploads/CV_Media/images/ \
 	-v $(pwd):/var/www/html/ \
-	-e "MYSQL_HOST=159.89.232.129" -e "MYSQL_DB=community_voices" -e "MYSQL_USER=public_cv" -e "MYSQL_PASS=1234" \
+	-e "MYSQL_HOST=159.89.232.129" -e "MYSQL_DB=community_voices" -e "MYSQL_USER=$user" -e "MYSQL_PASS=$pass" \
 	-e SERVER=`hostname` \
 	--name LOCAL_CV community-voices
 fi
