5a3ab2b595a176bf24c820c52250b72f06da17bf
jeremy <jeremy@jfein.net>
2019-01-04 17:03:31 -0600

    Add postfix

--- build/init.sh
+++ build/init.sh
@@ -2,7 +2,7 @@
 
 # install apt packages, set timezone (see: https://serverfault.com/a/683651/456938), install composer
 apt-get update && \
-	apt-get -qq -y install apt-utils tzdata apache2 php libapache2-mod-php php-cli php-mbstring php-xml php-mysql php-xdebug php-gd curl git unzip wget
+	apt-get -qq -y install apt-utils tzdata apache2 php libapache2-mod-php php-cli php-mbstring php-xml php-mysql php-xdebug php-gd curl git unzip wget postfix
 INI_LOC=`php -i | grep 'Loaded Configuration File => ' | sed 's/Loaded Configuration File => //g' | sed 's/cli/apache2/g'` && \
 	sed -ie 's/upload_max_filesize = 2M/upload_max_filesize = 64M/g' "$INI_LOC" && \
 	sed -ie 's/post_max_size = 8M/post_max_size = 512M/g' "$INI_LOC"
@@ -11,3 +11,4 @@ ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 	php composer.phar install && \
 	php composer.phar update
 a2enmod rewrite headers && mv /var/www/html/apache/000-default.conf /etc/apache2/sites-available/000-default.conf
+service postfix start
