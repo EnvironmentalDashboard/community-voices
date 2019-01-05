6ae1f7b5b94fd0a18e267ef12273c2e8ab6c47d9
jeremy <jeremy@jfein.net>
2019-01-05 14:57:47 -0600

    Removing incorrect implementation -- these attributes do not belong here

--- src/CommunityVoices/Model/Entity/Tag.php
+++ src/CommunityVoices/Model/Entity/Tag.php
@@ -9,34 +9,8 @@ class Tag extends Group
         $this->type = self::TYPE_TAG;
     }
 
-    private $mediaId;
-    private $groupId;
-
-    public function setMediaId(int $id)
-    {
-        $this->mediaId = $id;
-    }
-
-    public function getMediaId()
-    {
-        return $this->mediaId;
-    }
-
-    public function setGroupId(int $id)
-    {
-        $this->groupId = $id;
-    }
-
-    public function getGroupId()
-    {
-        return $this->groupId;
-    }
-
     public function toArray()
     {
-        return ['tag' => array_merge(parent::toArray()['group'], [
-            'mediaId' => $this->mediaId,
-            'groupId' => $this->groupId
-        ])];
+        return ['tag' => parent::toArray()['group']];
     }
 }
