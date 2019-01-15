545b5332a07b8cbd8e0a408c7d5890d4905b087b
Sam Craig <craigsamm@gmail.com>
2019-01-15 17:02:33 -0500

    Add incorporated view to Screen URLs page

---
+++ src/CommunityVoices/App/Website/Presentation/Module/Location.xslt
@@ -0,0 +1,50 @@
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+	version="1.0">
+
+	<xsl:output method="html" indent="yes" omit-xml-declaration="yes" />
+
+	<xsl:variable name="isManager" select="package/identity/user/role = 'manager'
+		or package/identity/user/role = 'administrator'"/>
+
+	<xsl:template match="/package">
+
+      <nav class="navbar navbar-light bg-light" style="justify-content:initial">
+        <a class="navbar-brand" href="/community-voices/" style="color:#28a745;font-family:'Multicolore',sans-serif">Community Voices</a>
+        <div class="float-left">
+          <ul class="navbar-nav" style="width:initial">
+            <li class="nav-item mr-2">
+              <a class="nav-link" href="../articles">Articles</a>
+            </li>
+            <li class="nav-item mr-2">
+              <a class="nav-link" href="../slides">Slides</a>
+            </li>
+            <li class="nav-item mr-2">
+              <a class="nav-link" href="../images">Images</a>
+            </li>
+            <li class="nav-item mr-2">
+              <a class="nav-link" href="../quotes">Quotes</a>
+            </li>
+          </ul>
+        </div>
+        <div style="margin-left:auto"></div>
+      </nav>
+
+      <div class="row">
+          <div class="col-12">
+              <h4 style="padding: 10px 20px 10px 20px">Locations</h4>
+
+              <ul>
+                  <xsl:for-each select="domain/locCollection/location">
+                      <li>
+                          <a href="/public/digital-signage.php?loc={id}">
+                              <xsl:value-of select="label" />
+                          </a>
+                      </li>
+                  </xsl:for-each>
+              </ul>
+          </div>
+      </div>
+
+	</xsl:template>
+
+</xsl:stylesheet>
--- src/CommunityVoices/App/Website/View/Location.php
+++ src/CommunityVoices/App/Website/View/Location.php
@@ -12,26 +12,83 @@ use Symfony\Component\HttpFoundation;
 
 class Location extends Component\View
 {
+    protected $recognitionAdapter;
+    protected $mapperFactory;
     protected $locationAPIView;
     protected $secureContainer;
+    protected $transcriber;
 
     public function __construct(
+        Component\RecognitionAdapter $recognitionAdapter,
+        Component\MapperFactory $mapperFactory,
         Api\View\Location $locationAPIView,
+        Component\Transcriber $transcriber,
         Api\Component\SecureContainer $secureContainer
     ) {
+        $this->recognitionAdapter = $recognitionAdapter;
+        $this->mapperFactory = $mapperFactory;
         $this->locationAPIView = $locationAPIView;
+        $this->transcriber = $transcriber;
         $this->secureContainer = $secureContainer;
     }
 
     public function getAllLocation($request)
     {
+        // Identity gathering
+        $identity = $this->recognitionAdapter->identify();
+
+        $identityXMLElement = new SimpleXMLElement(
+            $this->transcriber->toXml($identity->toArray())
+        );
+
+        // Location data gathering
         $locationAPIView = $this->secureContainer->contain($this->locationAPIView);
-        $json = json_decode($locationAPIView->getAllLocation()->getContent());
 
-        $response = new HttpFoundation\Response();
+        $locationXMLElement = new SimpleXMLElement(
+            $this->transcriber->toXml(json_decode(
+                $locationAPIView->getAllLocation()->getContent()
+            ))
+        );
 
-        $response->setContent(json_encode($json));
+        /**
+         * Location XML Package
+         */
+        $locationPackageElement = new Helper\SimpleXMLElementExtension('<package/>');
 
+        $packagedlocation = $locationPackageElement->addChild('domain');
+        $packagedlocation->adopt($locationXMLElement);
+
+        $packagedIdentity = $locationPackageElement->addChild('identity');
+        $packagedIdentity->adopt($identityXMLElement);
+
+        /**
+         * Generate Location module
+         */
+        $locationModule = new Component\Presenter('Module/Location');
+        $locationModuleXML = $locationModule->generate($locationPackageElement);
+
+        /**
+         * Get base URL
+         */
+        //$urlGenerator = new UrlGenerator($routes, $context);
+        //$baseUrl = $urlGenerator->generate('root');
+
+        /**
+         * Prepare template
+         */
+        $domainXMLElement = new Helper\SimpleXMLElementExtension('<domain/>');
+
+        $domainXMLElement->addChild('main-pane', $locationModuleXML);
+        //$domainXMLElement->addChild('baseUrl', $baseUrl);
+
+        $domainIdentity = $domainXMLElement->addChild('identity');
+        $domainIdentity->adopt($identityXMLElement);
+
+        $presentation = new Component\Presenter('SinglePane');
+
+        $response = new HttpFoundation\Response($presentation->generate($domainXMLElement));
+
+        $this->finalize($response);
         return $response;
     }
 }
