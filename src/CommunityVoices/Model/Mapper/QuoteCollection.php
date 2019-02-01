a2b522fed439d4f5265e33d1cdf6ccf0f3236957
jeremy <jeremy@jfein.net>
2019-02-01 16:28:48 -0500

    Revert "Thse collections implement Countable -- no need for count. Limit/page should be in MediaCollection."
    This reverts commit 7bddf7ab7b253645f055914d781fc468b9275f9e.

--- src/CommunityVoices/Model/Mapper/QuoteCollection.php
+++ src/CommunityVoices/Model/Mapper/QuoteCollection.php
@@ -140,6 +140,8 @@ class QuoteCollection extends DataMapper
 
         $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
+        $quoteCollection->setCount($this->conn->query('SELECT FOUND_ROWS()')->fetchColumn());
+
         foreach ($results as $key => $entry) {
             $quoteCollection->addEntityFromParams($entry);
         }
