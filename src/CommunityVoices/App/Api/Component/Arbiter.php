cc14d3e9432f29f29823fb6ca4e4349aad7bbf93
jeremy <jeremy@jfein.net>
2019-02-13 23:46:21 -0500

    some (helpful) comments

--- src/CommunityVoices/App/Api/Component/Arbiter.php
+++ src/CommunityVoices/App/Api/Component/Arbiter.php
@@ -8,8 +8,13 @@ namespace CommunityVoices\App\Api\Component;
  *
  * The specific rules that the Arbiter reads and enacts are stored in
  * App/Api/Component/Config/AccessControlList.json
+ * 
+ * For rules:
+ * - Any specific rule takes priority over a pattern matches
+ * - Pattern matches use UNIX filename patterns (see man for `fnmatch`)
  *
  * @todo This needs more thought -- there needs to be higher security
+ * @todo Potentially deprecate and find a good object-oriented RBAC package
  */
 
 use CommunityVoices\Model\Entity;
@@ -20,6 +25,12 @@ class Arbiter
 
     protected $roles;
 
+    /**
+     * Creates an arbiter based on the provided rules and roles
+     *
+     * @param array $roles
+     * @param array $rules
+     */
     public function __construct($roles, $rules)
     {
         $this->rules = $rules;
@@ -34,6 +45,13 @@ class Arbiter
         }
     }
 
+    /**
+     * Checks if the provided $signature is allowed for $user
+     *
+     * @param string $signature Class signature as provided by `get_class`
+     * @param User $user Valid pre-mapped user entity
+     * @return boolean True if the user is not denied access to the provided signature 
+     */
     public function isAllowedForIdentity($signature, Entity\User $user)
     {
         $rule = $this->matchRuleBySignature($signature);
@@ -51,12 +69,18 @@ class Arbiter
 
     private function matchRuleBySignature($needle)
     {
+        /**
+         * <Priority!> Check specific rules
+         */
         foreach ($this->rules as $key => $rule) {
             if ($rule[0] === $needle) {
                 return $rule;
             }
         }
 
+        /**
+         * Check pattern-based rules
+         */
         foreach ($this->rules as $key => $rule) {
             if (fnmatch($rule[0], $needle, FNM_NOESCAPE)) {
                 return $rule;
