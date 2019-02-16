7f3703f7e01a1d1a2c6590da85ca4a6571b35456
jeremy <jeremy@jfein.net>
2019-02-16 18:37:07 -0500

    UrlGenerator provider

---
+++ src/CommunityVoices/App/Website/Bootstrap/Provider/UrlGenerator.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace CommunityVoices\App\Website\Bootstrap\Provider;
+
+use CommunityVoices\App\Website\Component\Provider;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * @overview Url generator providerc
+ */
+
+class UrlGenerator extends Provider {
+    protected $routes;
+    protected $request;
+
+    public function __construct($injector, RouteCollection $routes, $request)
+    {
+        parent::__construct($injector);
+
+        $this->routes = $routes;
+        $this->request = $request;
+    }
+
+    public function init()
+    {
+        $context = new \Symfony\Component\Routing\RequestContext();
+        $context->fromRequest($this->request);
+
+        $urlGenerator = new \Symfony\Component\Routing\Generator\UrlGenerator($this->routes, $context);
+        $this->injector->share($urlGenerator);
+    }
+}
