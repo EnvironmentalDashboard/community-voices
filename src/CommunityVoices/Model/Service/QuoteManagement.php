373ce3799fcbbc186a8aada5349998c14431ec85
Sam Craig <craigsamm@gmail.com>
2019-04-11 22:01:57 -0400

    Allow uploading quotes by setting addedBy

--- src/CommunityVoices/Model/Service/QuoteManagement.php
+++ src/CommunityVoices/Model/Service/QuoteManagement.php
@@ -56,6 +56,7 @@ class QuoteManagement
         $quote->setAttribution($attribution);
         $quote->setSubAttribution($subAttribution);
         $quote->setDateRecorded($dateRecorded);
+        $quote->setAddedBy($addedBy);
         if ($approved) {
             $quote->setStatus(3);
         } else {
