0341a879e6e4fe5e417868d04d07c287357814e4
Sam Craig <craigsamm@gmail.com>
2019-04-27 08:56:36 -0400

    Document the DisplayError paradigm exception

--- src/CommunityVoices/App/Api/Controller/DisplayError.php
+++ src/CommunityVoices/App/Api/Controller/DisplayError.php
@@ -6,6 +6,12 @@ use CommunityVoices\App\Api\Component;
 
 class DisplayError extends Component\Controller
 {
+    /*
+     * Note: This is intentionally a public method.
+     * Do not make it private!
+     * It will cause an infinite loop, as it will be wrapped in a
+     * SecureContainer forever.
+     */
     public function getError()
     {
 
