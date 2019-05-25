babbb09d1b785d7a88c3d639449d33079d9c766e
Sam Craig <craigsamm@gmail.com>
2019-05-25 10:25:33 -0400

    Add ContentCategoryManagement service

---
+++ src/CommunityVoices/Model/Service/ContentCategoryManagement.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace CommunityVoices\Model\Service;
+
+use CommunityVoices\Model\Entity;
+use CommunityVoices\Model\Component;
+use CommunityVoices\Model\Mapper;
+use CommunityVoices\Model\Exception;
+
+class ContentCategoryManagement
+{
+    private $mapperFactory;
+    private $stateObserver;
+
+    public function __construct(
+        Component\MapperFactory $mapperFactory,
+        Component\StateObserver $stateObserver
+    ) {
+        $this->mapperFactory = $mapperFactory;
+        $this->stateObserver = $stateObserver;
+    }
+
+    public function upload(
+        Entity\Image $image,
+        $label
+    ) {
+        $contentCategory = new Entity\ContentCategory;
+
+        $contentCategory->setImage($image);
+        $contentCategory->setLabel($label);
+
+        // Error observer
+        $this->stateObserver->setSubject('contentCategoryUpload');
+        $contentCategory->validateForUpload($this->stateObserver);
+
+        $clientState = $this->mapperFactory->createClientStateMapper(Mapper\ClientState::class);
+        $contentCategoryMapper = $this->mapperFactory->createDataMapper(Mapper\ContentCategory::class);
+
+        if ($this->stateObserver->hasEntries()) {
+            $clientState->save($this->stateObserver);
+            return false;
+        }
+
+        $contentCategoryMapper->save($contentCategory);
+        return true;
+    }
+
+    public function update($groupId, $label)
+    {
+        $contentCategoryMapper = $this->mapperFactory->createDataMapper(Mapper\ContentCategory::class);
+
+        $contentCategory = new Entity\ContentCategory;
+        $contentCategory->setGroupId((int) $groupId);
+
+        $contentCategoryMapper->fetch($contentCategory);
+
+        $contentCategory->setLabel($label);
+
+        // Error observer
+        $this->stateObserver->setSubject('contentCategoryUpdate');
+        $contentCategory->validateForUpload($this->stateObserver);
+
+        $clientState = $this->mapperFactory->createClientStateMapper(Mapper\ClientState::class);
+
+        if (!$isValid || $this->stateObserver->hasEntries()) {
+            $clientState->save($this->stateObserver);
+            return false;
+        }
+
+        $contentCategoryMapper->save($contentCategory);
+        return true;
+    }
+}
