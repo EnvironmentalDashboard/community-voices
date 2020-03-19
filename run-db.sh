f3c43f8dfc084e5d894729c934943da02e761274
Sam Craig <craigsamm@gmail.com>
2020-03-19 19:24:48 -0400

    Switch to empty password for local MySQL

--- run-db.sh
+++ run-db.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-docker run -p 3007:3306 -e "MYSQL_ROOT_HOST=172.17.0.1" -e "MYSQL_ROOT_PASSWORD=root" --name cv-mysql -d mysql/mysql-server:latest
+docker run -p 3007:3306 -e "MYSQL_ROOT_HOST=172.17.0.1" -e "MYSQL_ALLOW_EMPTY_PASSWORD=true" --name cv-mysql -d mysql/mysql-server:latest
 
-# Then, connect with `mysql -h 127.0.0.1 -P 3007 -u root -p`
-# password will be `root`.
-# Consider empty password?
+# Then, connect with `mysql -h 127.0.0.1 -P 3007 -u root`
