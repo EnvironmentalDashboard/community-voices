a227098a9cddb29f7d668346adb9e67fd280c7e8
Sam Craig <craigsamm@gmail.com>
2020-03-20 10:49:06 -0400

    Use the same MySQL version locally as we use in production

--- run-db.sh
+++ run-db.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-docker run -p 3007:3306 -e "MYSQL_ROOT_HOST=%" -e "MYSQL_ALLOW_EMPTY_PASSWORD=true" --name cv-mysql -d mysql/mysql-server:latest
+docker run -p 3007:3306 -e "MYSQL_ROOT_HOST=%" -e "MYSQL_ALLOW_EMPTY_PASSWORD=true" --name cv-mysql -d mysql/mysql-server:5.7
 
 # Then, connect with `mysql -h 127.0.0.1 -P 3007 -u root`
