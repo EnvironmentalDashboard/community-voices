99975435ec3a17dc55d8cb476c101aae6521415d
Sam Craig <craigsamm@gmail.com>
2020-03-20 10:32:29 -0400

    Allow any connections to local database

--- run-db.sh
+++ run-db.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-docker run -p 3007:3306 -e "MYSQL_ROOT_HOST=172.17.0.1" -e "MYSQL_ALLOW_EMPTY_PASSWORD=true" --name cv-mysql -d mysql/mysql-server:latest
+docker run -p 3007:3306 -e "MYSQL_ROOT_HOST=%" -e "MYSQL_ALLOW_EMPTY_PASSWORD=true" --name cv-mysql -d mysql/mysql-server:latest
 
 # Then, connect with `mysql -h 127.0.0.1 -P 3007 -u root`
