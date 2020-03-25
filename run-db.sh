4c023362df9821683d46f46927dc72886822becc
Sam Craig <craigsamm@gmail.com>
2020-03-25 13:51:06 -0400

    Upon inspection, it seems 35 seconds will do

--- run-db.sh
+++ run-db.sh
@@ -4,6 +4,6 @@ docker run -p 3007:3306 -e "MYSQL_ROOT_HOST=%" -e "MYSQL_ALLOW_EMPTY_PASSWORD=tr
 
 # Then, connect with `mysql -h 127.0.0.1 -P 3007 -u root`
 # I wonder if we could more programatically do this.
-echo "Waiting a minute for mysql server to start up..."
-sleep 60
+echo "Waiting half a minute for mysql server to start up..."
+sleep 35
 mysql -h 127.0.0.1 -P 3007 -u root < migrate/schema.sql
