062c75ea5f7589d0a39d491cf6721ad774630519
Sam Craig <craigsamm@gmail.com>
2020-03-25 16:18:29 -0400

    Missed some crucial Docker arguments in the database running command

--- run-db.sh
+++ run-db.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-docker run -p 3007:3306 -e "MYSQL_ROOT_HOST=%" -e "MYSQL_ALLOW_EMPTY_PASSWORD=true" --name cv-mysql -d mysql/mysql-server:5.7
+docker run -dit --restart always -p 3007:3306 -e "MYSQL_ROOT_HOST=%" -e "MYSQL_ALLOW_EMPTY_PASSWORD=true" --name cv-mysql mysql/mysql-server:5.7
 
 # Then, connect with `mysql -h 127.0.0.1 -P 3007 -u root`
 # I wonder if we could more programatically do this.
