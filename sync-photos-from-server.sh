51a1ba7502a0ee2ac4aa264ae5abf8f70bc0ed04
Sam Craig <craigsamm@gmail.com>
2020-03-26 19:43:35 -0400

    Allow specifying a custom IP to get photos from

--- sync-photos-from-server.sh
+++ sync-photos-from-server.sh
@@ -1,1 +1,10 @@
-rsync -av root@159.89.232.129:/var/www/uploads/CV_Media/ ./CV_Media
+# If a different IP should be used, it can be provided
+# as the first command-line argument.
+if [ "$#" -gt 0 ]
+then
+  ip=$1
+else
+  ip=159.89.232.129
+fi
+
+rsync -av root@$ip:/var/www/uploads/CV_Media/ ./CV_Media
