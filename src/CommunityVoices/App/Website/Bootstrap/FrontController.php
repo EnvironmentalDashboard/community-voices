d27c1a9f555ebc81b547b02c618aca146fb28347
Ethan Michel <emichel2@oberlin.edu>
2020-06-04 10:54:08 -0600

    updated catch block and indentation

--- src/CommunityVoices/App/Website/Bootstrap/FrontController.php
+++ src/CommunityVoices/App/Website/Bootstrap/FrontController.php
@@ -52,19 +52,18 @@ class FrontController
     public function doRequest($request)
     {
         $this->loadProviders($request);
-          try {
+        try {
             $this->router->route($request);
             $this->dispatcher->dispatch($request)->send();
-          } catch (\Throwable $t) {
+        } catch (\Throwable $t) {
             // if the server is local, we want to be able to see the error stack,
             // but on remote server we want to have an error page display instead
             if (getenv('APP_ENV') == 'development') {
-              $this->router->route($request);
-              $this->dispatcher->dispatch($request)->send();
+                throw $t;
             } else {
-              $this->fail($request, $t)->send();
+                $this->fail($request, $t)->send();
             }
-          }
+        }
     }
 
     protected function loadProviders($request)
