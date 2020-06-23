8f436634ab2ff857610c239c536a0530e4cc23be
Ethan Michel <emichel2@oberlin.edu>
2020-06-23 14:51:28 -0600

    cleaner javascript

--- src/CommunityVoices/Public/js/quote-form.js
+++ src/CommunityVoices/Public/js/quote-form.js
@@ -14,12 +14,12 @@ function clickEdited() {
 }
 
 function displayCount(box,count) {
-  var countDisplay = document.getElementById(count);
+  const countDisplay = document.getElementById(count);
+  countDisplay.innerHTML = box.value.length + " / " + box.getAttribute("maxlength") + " characters ";
   if(box.hasAttribute("suggestedlength") && (box.value.length > box.getAttribute("suggestedlength"))) {
-    countDisplay.innerHTML = box.value.length + " / " + box.getAttribute("maxlength") + " characters -- Please shorten!";
+    countDisplay.insertAdjacentHTML('beforeend', '-- Please Shorten!');
     countDisplay.style.color = "red";
   } else {
-    countDisplay.innerHTML = box.value.length + " / " + box.getAttribute("maxlength") + " characters";
     countDisplay.style.color = "black";
   }
 }
