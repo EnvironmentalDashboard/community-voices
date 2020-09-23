5bbf345e3cad36d499b2451853dc233cb12c3d58
Sam Craig <craigsamm@gmail.com>
2020-09-22 21:25:28 -0400

    Periodically delete old sessions
    Otherwise, hosting server will run out of inodes

--- build/init.sh
+++ build/init.sh
@@ -2,7 +2,7 @@
 
 # install apt packages, set timezone (see: https://serverfault.com/a/683651/456938), install composer
 apt-get update && \
-	apt-get -qq -y install apt-utils tzdata apache2 php libapache2-mod-php php-cli php-mbstring php-xml php-mysql php-xdebug php-gd curl php-curl git unzip wget postfix
+	apt-get -qq -y install apt-utils tzdata apache2 php libapache2-mod-php php-cli php-mbstring php-xml php-mysql php-xdebug php-gd curl php-curl git unzip wget postfix cron
 INI_LOC=`php -i | grep 'Loaded Configuration File => ' | sed 's/Loaded Configuration File => //g' | sed 's/cli/apache2/g'` && \
 	sed -ie 's/upload_max_filesize = 2M/upload_max_filesize = 64M/g' "$INI_LOC" && \
 	sed -ie 's/post_max_size = 8M/post_max_size = 512M/g' "$INI_LOC"
@@ -21,3 +21,6 @@ chmod a+w /var/www/html/log/access.log
 
 # Make sure CV_Media/images exists
 mkdir -p /var/www/html/CV_Media/images
+
+# Load cron file
+crontab /var/www/html/crontab/crontab
