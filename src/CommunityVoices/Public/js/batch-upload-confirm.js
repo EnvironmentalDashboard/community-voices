7ecb90a303f5ee911d64bf906d28cb44157e9a03
Ethan Michel <emichel2@oberlin.edu>
2020-10-29 21:14:47 -0600

    feedback for incorrect content categories

--- src/CommunityVoices/Public/js/batch-upload-confirm.js
+++ src/CommunityVoices/Public/js/batch-upload-confirm.js
@@ -52,7 +52,8 @@ function fillCheckBoxes (listSelected, allBoxes) { // takes content category fie
    });
 }
 
-function checkFieldEmpty(row) { // checks if a required/suggested field (edited quotes,content categories, attributions) is empty or not
+function checkFieldEmpty(row, givenContentCategories = null) { // checks if a required/suggested field (edited quotes,content categories, attributions) is empty or not
+    if (givenContentCategories !== null) console.log(givenContentCategories);
    if (! row.closest("#unpairedQuotes").length) { // issues will only be checked for paired quotes, we will ignore issues with unpaired quotes.
        rowType = row.find(".checkboxHeader").length != 0 ? "checkbox" : "field";
        linkExists = row.parent("a").length; // have we already added a link to this? Need to check
@@ -62,8 +63,11 @@ function checkFieldEmpty(row) { // checks if a required/suggested field (edited 
        quoteNumber = row.closest("[quoteNumber]").length ? "quote " + row.closest("[quoteNumber]").attr("quoteNumber") : "";
        columnName = row.attr("formattedName");
 
+       strGivenCCs = givenContentCategories === null ? '' : " (from sheet: " + givenContentCategories.map(cc => cc.innerHTML).filter(cc => cc.match("^[A-Za-z0-9]+$")).join() + ')';
+
        strToAdd = identifier + " " + quoteNumber + " " + columnName;
        linkToAdd = strToAdd.split(' ').join('');
+       strToAdd += strGivenCCs;
 
        isEmpty = rowType == "checkbox" ? row.find($('input:checkbox:checked')).length == 0 : input.val().length == 0
 
@@ -179,7 +183,8 @@ $(document).ready(function() {
         fillCheckBoxes(listSelected[1],allBoxes[1]);
     });
     $("[message]").each(function() {
-        checkFieldEmpty($(this));
+        ($(this).attr("formattedName") == "contentcategories") ? givenContentCategories = $(this).find("li").toArray() : givenContentCategories = null;
+        checkFieldEmpty($(this),givenContentCategories);
     });
     checkEntryIssuesEmpty();
     $(".individualSource").each(function () {
