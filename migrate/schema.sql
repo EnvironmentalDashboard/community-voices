459b86226e81b081dfb78af42b7930568141f6fd
Sam Craig <craigsamm@gmail.com>
2020-10-31 11:31:08 -0400

    Found issue, but not getting fixed yet

--- migrate/schema.sql
+++ migrate/schema.sql
@@ -389,4 +389,4 @@ CREATE TABLE `community-voices_users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-31 10:57:05
+-- Dump completed on 2020-10-31 11:29:32
